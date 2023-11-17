53e924e1927212fd3fc88f3a72cdb593342c424f
Pooja <poojaoctober11@gmail.com>
2023-11-17 23:21:37 +0530

    initial commit

--- app/src/main/java/com/example/cryptoapp/Front.kt
+++ app/src/main/java/com/example/cryptoapp/Front.kt
@@ -13,7 +13,7 @@ class Front : AppCompatActivity() {
         setContentView(R.layout.activity_front)
 
         Handler(Looper.getMainLooper()).postDelayed(3000){
-            startActivity(Intent(this,MainActivity::class.java))
+            startActivity(Intent(this,VarificationActivity::class.java))
             finish()
         }
     }
